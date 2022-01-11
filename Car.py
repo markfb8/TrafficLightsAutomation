6cab44af6313357b6a68ab866ce5a2e72133d7b8
Daniel98SP <daniel98sp@gmail.com>
2022-01-11 12:55:30 +0100

    Car turn bug fixed

--- Car.py
+++ Car.py
@@ -17,6 +17,7 @@ class Car:
         random_number = random.uniform(0, 1)
 
         if (self.num_of_direction_changes == 0 and random_number > 0.75) or (self.num_of_direction_changes == 1 and random_number > 0.90):
+            self.num_of_direction_changes += 1
             return 'VERTICAL' if direction == 'HORIZONTAL' else 'HORIZONTAL'
         else:
             return direction
