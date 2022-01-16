fe5b3c1cebe585e4f9a3fb2d2a7653f4ac2edb8a
Daniel98SP <daniel98sp@gmail.com>
2022-01-16 19:34:10 +0100

    .

--- MapManager.py
+++ MapManager.py
@@ -74,6 +74,6 @@ def calculate_added_time(simulation, direction):
         time = randint(3, 6)
 
     if direction == 'VERTICAL':
-        return time * simulation.vertical_density
+        return time * simulation.vertical_density * 2
     else:
-        return time * (1 - simulation.vertical_density)
+        return time * (1 - simulation.vertical_density) * 2
